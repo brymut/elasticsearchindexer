5d5ec7755f4a90ebf9866f848b45f6527da5cc9b
brymut <mutaiwork@gmail.com>
2017-01-23 11:24:42 +0000

    quick trannsfer

---
+++ elasticsearch_indexer/Document.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace elasticsearch_indexer
+{
+    public class Document
+    {
+        public string author { get; set; }
+        public DateTime creation_date { get; set; }
+        public string file_name { get; set; }
+        public string title { get; set; }
+        //public string description { get; set; }
+
+
+        public Document( string Author, DateTime creationDate, string fileName, string Title)
+        {
+            author = Author;
+            creation_date = creationDate;
+            file_name = fileName;
+            title = Title;
+           // description = Description;
+
+
+        }
+    }
+
+  
+}
---
+++ elasticsearch_indexer/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nest;
+using System.IO;
+
+namespace elasticsearch_indexer
+{
+    class Program
+    {
+        public static Uri node;
+        public static ConnectionSettings settings;
+        public static ElasticClient client;
+        static void Main(string[] args)
+        {
+            node = new Uri("http://localhost:9200");
+            settings = new ConnectionSettings(node).
+                DefaultIndex("avaloq_docs");
+            client = new ElasticClient(settings);
+
+            //InsertData();
+            SimpleQuery("apple");
+
+            
+
+        }
+
+        public static void InsertData()
+        {
+            List<Document> documents = new List<Document>();
+
+            foreach (string line in File.ReadLines(@"C:\Users\mutai_000\indexing_helper.txt"))
+            {
+                String[] line_text = line.Split(',');
+                var doc = new Document(line_text[2], Convert.ToDateTime(line_text[3]), line_text[0], line_text[1]);
+                documents.Add(doc);
+                //Console.WriteLine(doc);
+            }
+            foreach (var item in documents)
+            {
+                client.Index(item);
+                Console.WriteLine(item);
+            }
+
+
+        }
+
+        public static void SimpleQuery(string term) {
+
+            var result = client.Search<Document>(s => s
+                                .Query(p => p.Term(q => q.title, term)));
+
+            //Console.WriteLine(result.Documents);
+ 
+        }
+    }
+}
