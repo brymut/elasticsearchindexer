36bcdb65cf11d96df3c0e0cbb7ecd5e748671527
brymut <mutaiwork@gmail.com>
2017-03-08 14:08:23 +0000

    More data

--- elasticsearch_indexer/Document.cs
+++ elasticsearch_indexer/Document.cs
@@ -1,31 +1,92 @@
-﻿using System;
+﻿using AvaloqDocu.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Web;
 
-namespace elasticsearch_indexer
+namespace AvaloqDocu.Models
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class Document
     {
-        public string author { get; set; }
-        public DateTime creation_date { get; set; }
-        public string file_name { get; set; }
-        public string title { get; set; }
-        //public string description { get; set; }
 
-
-        public Document( string Author, DateTime creationDate, string fileName, string Title)
+        /// <summary>
+        ///  Initialises a new instance of the Document class.
+        /// </summary>
+        /// <param name="Title"
+        /// <param name="Subtitle"
+        /// <param name="DocuID"
+        /// <param name="Release"
+        /// <param name="FunctionalArea"
+        /// <param name="DocuType"
+        /// <param name="SubType"
+        /// <param name="LastModified"
+        /// <param name="Filepath"
+        public Document(string Title = null, string Subtitle = null, int DocuID = 0, string Release = null, string FunctionalArea = null, string DocuType = null, string SubType = null, DateTime? LastModified = null, string Filepath = null)
         {
-            author = Author;
-            creation_date = creationDate;
-            file_name = fileName;
-            title = Title;
-           // description = Description;
-
+            this.Title = Title;
+            this.Subtitle = Subtitle;
+            this.DocuID = DocuID;
+            this.Release = Release;
+            this.FunctionalArea = FunctionalArea;
+            this.DocuType = DocuType;
+            this.SubType = SubType;
+            this.LastModified = LastModified ?? DateTime.MinValue;
+            this.FilePath = FilePath;
 
         }
-    }
 
-  
+
+
+        public int DocumentID { get; set; }
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public int DocuID { get; set; }
+        public string Release { get; set; }
+        public string FunctionalArea { get; set; }
+        public string DocuType { get; set; }
+        public string SubType { get; set; }
+        public DateTime LastModified { get; set; }
+        public int FilePathId { get; set; }
+        public int FileSize { get; set; }
+        public virtual string FilePath { get; set; }
+
+        /// <summary>
+        ///  Hash code implementation, that might help with finding duplicates later on.
+        /// </summary>
+        public override int GetHashCode()
+        {
+            // http://stackoverflow.com/a/263416/677735
+
+            unchecked
+            {
+                int hash = 41;
+
+                if (this.Title != null)
+                    hash = hash * 59 + this.Title.GetHashCode();
+                if (this.Subtitle != null)
+                    hash = hash * 59 + this.Subtitle.GetHashCode();
+                // figure something out about the tags.
+                //if (this.DocuID != null)
+                hash = hash * 59 + this.DocuID.GetHashCode();
+                if (this.Release != null)
+                    hash = hash * 59 + this.Release.GetHashCode();
+                if (this.FunctionalArea != null)
+                    hash = hash * 59 + this.FunctionalArea.GetHashCode();
+                if (this.DocuType != null)
+                    hash = hash * 59 + this.DocuType.GetHashCode();
+                if (this.SubType != null)
+                    hash = hash * 59 + this.SubType.GetHashCode();
+                if (this.LastModified != null)
+                    hash = hash * 59 + this.LastModified.GetHashCode();
+                if (this.FilePath != null)
+                    hash = hash * 59 + this.LastModified.GetHashCode();
+
+                return hash;
+            }
+        }
+
+    }
 }
--- elasticsearch_indexer/Program.cs
+++ elasticsearch_indexer/Program.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Nest;
 using System.IO;
+using AvaloqDocu.Models;
+using System.Globalization;
 
 namespace elasticsearch_indexer
 {
@@ -17,13 +19,96 @@ namespace elasticsearch_indexer
         {
             node = new Uri("http://localhost:9200");
             settings = new ConnectionSettings(node).
-                DefaultIndex("avaloq_docs");
+                DefaultIndex("avaloq_documents");
             client = new ElasticClient(settings);
 
             //InsertData();
-            SimpleQuery("apple");
+            int DocuID = 0;
+            string Release = null;
+            string FunctionalArea = null;
+            string DocuType = "Avaloq Core - System Documentation";
+            string SubType = null;
+            //DateTime? LastModifiedFrom = new DateTime(2016,01,01);
+            //DateTime? LastModifiedTo = new DateTime(2017,01,01);
+            DateTime? LastModifiedFrom = null;
+            DateTime? LastModifiedTo = null;
+
+
+            var filters = new List<Func<QueryContainerDescriptor<Document>, QueryContainer>>();
+            
+            if (DocuID != 0)
+            {
+                filters.Add(r2 => r2.Match(q2 => q2
+                                           .Query(DocuID.ToString())
+                                                .Field(f3 => f3.DocuID)));
+            }
+
+            if ( FunctionalArea != null)
+            {
+                filters.Add(r2 => r2.Match(q2 => q2
+                                            .Query(FunctionalArea)
+                                                 .Field(f3 => f3.FunctionalArea)));
+            }
+
+            if (Release != null)
+            {
+                filters.Add(r2 => r2.Match(q2 => q2
+                                           .Query(Release+".0.0.0")
+                                                .Field(f3 => f3.Release)));
+            }
+
+            if (DocuType != null)
+            {
+                filters.Add(r2 => r2.Match(q2 => q2
+                                           .Query(DocuType)
+                                                .Field(f3 => f3.DocuType)));
+            }
+
+
+            var result = client.Search<Document>(x => x
+                            .Query(q => q
+                                .MultiMatch(mp => mp
+                                    .Query("Swift")
+                                        .Fields(f => f
+                                            .Fields(f1 => f1.Title, f2 => f2.Subtitle))))
+                                            .PostFilter(r => r
+                                                .Bool(r1 => r1.Must(filters) 
+                                                    )));
+
+
+
+            var postFilter = result.Documents.AsQueryable();
+
+
 
             
+
+
+            if (SubType != null)
+            {
+                postFilter = postFilter.Where(x => x.SubType == SubType);
+            }
+
+
+
+            if (LastModifiedFrom != null)
+            {
+                postFilter = postFilter.Where(x => x.LastModified >= LastModifiedFrom);
+            }
+
+
+
+            if (LastModifiedTo != null)
+            {
+                postFilter = postFilter.Where(x => x.LastModified <= LastModifiedTo);
+            }
+
+
+
+
+            var final = postFilter.OrderByDescending(r1 => r1.LastModified);
+
+    
 
         }
 
@@ -31,29 +116,38 @@ namespace elasticsearch_indexer
         {
             List<Document> documents = new List<Document>();
 
-            foreach (string line in File.ReadLines(@"C:\Users\mutai_000\indexing_helper.txt"))
+            foreach (string line in File.ReadLines(@"c:/avaloq_docs.txt", Encoding.UTF8))
             {
-                String[] line_text = line.Split(',');
-                var doc = new Document(line_text[2], Convert.ToDateTime(line_text[3]), line_text[0], line_text[1]);
-                documents.Add(doc);
-                //Console.WriteLine(doc);
-            }
-            foreach (var item in documents)
-            {
-                client.Index(item);
-                Console.WriteLine(item);
+                string[] docInfo = line.Split(',');
+                int day, month, year;
+                try
+                {
+                    day = int.Parse(docInfo[3].Substring(0, 2));
+                    month = int.Parse(docInfo[3].Substring(3, 2));
+                    year = int.Parse(docInfo[3].Substring(6, 2)) + 2000;
+
+                }
+                catch
+                {
+                    continue;
+                }
+
+                
+                var document = new Document(docInfo[0], docInfo[1], int.Parse(docInfo[6]), docInfo[7], docInfo[5], docInfo[2], docInfo[4],new DateTime(year,month,day));
+                documents.Add(document);
             }
 
+
+           var result = client.IndexMany(documents);
 
         }
 
-        public static void SimpleQuery(string term) {
+        public static void SimpleQuery(string term)
+{
 
-            var result = client.Search<Document>(s => s
-                                .Query(p => p.Term(q => q.title, term)));
+    
+    //Console.WriteLine(result.Documents);
 
-            //Console.WriteLine(result.Documents);
- 
-        }
+}
     }
 }
